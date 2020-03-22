1b37faf9875483346a3a86b9a4f3e9566a0ad41b
Santi Noacco <santinoacco@gmail.com>
2020-03-22 16:33:30 -0300

    add plot frec_of_ill vs time

--- simu.py
+++ simu.py
@@ -21,30 +21,48 @@ plt.axis([0, 1, 0, 1])
 red_patch = mpatches.Patch(color='red', label='Sano')
 blue_patch = mpatches.Patch(color='blue', label='Infectado')
 
-for j in range(100):
-    velocidades = [[rd.random(), rd.random(), 0] for j in range(num_personas)]
+#keepgoing = True
+#while keepgoing:
+t_lapse=[]
+tot_contagiados=[]
+for t in range(100):
+    velocidades = [[rd.random(), rd.random(), 0] for t in range(num_personas)]
     velocidades = pd.DataFrame(velocidades, columns=['x', 'y', 'sano'])
     velocidades.loc[:,['x','y']] = velocidades.loc[:,['x','y']] - 0.5
 
     personas = personas + step_vel*velocidades
 
     personas[["x", 'y']] = personas[["x", 'y']] % 1
-
-    contagiables_distancia = euclidean_distances(personas[['x', 'y']]) - np.identity(num_personas)
-    contagiables_distancia = (contagiables_distancia < distancia_contagiable) & (contagiables_distancia != -1)
+    
+    tot_contagiados.append((num_personas - personas['sano'].sum())/num_personas)
+    
+        
+    # --Check threshold distance between people.
+    contagiable_dist = euclidean_distances(personas[['x', 'y']]) - np.identity(num_personas)
+    contagiable_dist = (contagiable_dist < distancia_contagiable) & (contagiable_dist != -1)
 
     posibilidad_contagio = np.tensordot(personas["sano"], personas["sano"], axes=0)
     posibilidad_contagio = posibilidad_contagio == 0
 
-    a_contagiar = posibilidad_contagio & contagiables_distancia
+    a_contagiar = posibilidad_contagio & contagiable_dist
     a_contagiar = np.argwhere(a_contagiar == True)
     a_contagiar = set(a_contagiar.flatten())
 
     personas.loc[a_contagiar, 'sano'] = 0
 
+    plt.figure(1)
     plt.axis([0, 1, 0, 1])
     plt.scatter(personas['x'], personas['y'], c=personas['sano'])
 
     plt.legend(handles=[red_patch, blue_patch], loc='upper left')
+    plt.pause(0.2)
+    plt.cla()
+
+    t_lapse.append(t)
+    
+    plt.figure(2)
+    plt.plot(t_lapse,tot_contagiados,'r+')
+    plt.ylabel(f'frecuencia contagiados')
+    plt.xlabel(f'tiempo [pasos MC]')
     plt.pause(0.2)
     plt.cla()
