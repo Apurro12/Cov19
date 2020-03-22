a50af7399b8052a971772a2d7c660479eac5fb54
camilo.leonel.amadio <camilo.leonel.amadio@accenture.com>
2020-03-22 19:03:50 -0300

    Cambie el feature 'sano' a 'enfermo' tambien cambie los colores para que tengan sentido

--- simu.py
+++ simu.py
@@ -6,24 +6,24 @@ import matplotlib.patches as mpatches
 from sklearn.metrics.pairwise import euclidean_distances
 
 #Preparo la simulacion
-num_personas = 60
-distancia_contagiable = 0.1
-step_vel = 0.02
+num_personas = 1000
+distancia_contagiable = 0.05
+step_vel = 0.01
 
 personas = [rd.random(size=3) for j in range(num_personas)]
-personas = pd.DataFrame(personas,columns = ['x','y','sano'])
-personas.loc[:,'sano'] = personas.loc[:,'sano'].apply(lambda x:1 if x < 0.8  else 0 )
+personas = pd.DataFrame(personas,columns = ['x','y','enfermo'])
+personas.loc[:,'enfermo'] = personas.loc[:,'enfermo'].apply(lambda x:1 if x > 0.995  else 0 )
 
 
 plt.ion()
 plt.axis([0, 1, 0, 1])
 
-red_patch = mpatches.Patch(color='red', label='Sano')
+red_patch = mpatches.Patch(color='red', label='enfermo')
 blue_patch = mpatches.Patch(color='blue', label='Infectado')
 
 for j in range(100):
     velocidades = [[rd.random(), rd.random(), 0] for j in range(num_personas)]
-    velocidades = pd.DataFrame(velocidades, columns=['x', 'y', 'sano'])
+    velocidades = pd.DataFrame(velocidades, columns=['x', 'y', 'enfermo'])
     velocidades.loc[:,['x','y']] = velocidades.loc[:,['x','y']] - 0.5
 
     personas = personas + step_vel*velocidades
@@ -33,18 +33,16 @@ for j in range(100):
     contagiables_distancia = euclidean_distances(personas[['x', 'y']]) - np.identity(num_personas)
     contagiables_distancia = (contagiables_distancia < distancia_contagiable) & (contagiables_distancia != -1)
 
-    posibilidad_contagio = np.tensordot(personas["sano"], personas["sano"], axes=0)
-    posibilidad_contagio = posibilidad_contagio == 0
+    a_contagiar = contagiables_distancia @ personas["enfermo"]
+    a_contagiar = np.argwhere(a_contagiar > 0).flatten()
 
-    a_contagiar = posibilidad_contagio & contagiables_distancia
-    a_contagiar = np.argwhere(a_contagiar == True)
-    a_contagiar = set(a_contagiar.flatten())
-
-    personas.loc[a_contagiar, 'sano'] = 0
+    personas.loc[a_contagiar,"enfermo"] = 1
 
     plt.axis([0, 1, 0, 1])
-    plt.scatter(personas['x'], personas['y'], c=personas['sano'])
+    plt.scatter(personas['x'], personas['y'], c=personas['enfermo'].apply(lambda x: 'red' if x==1 else 'blue'))
 
     plt.legend(handles=[red_patch, blue_patch], loc='upper left')
     plt.pause(0.2)
     plt.cla()
+
+    print(personas['enfermo'].sum())
